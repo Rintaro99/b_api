02cf7019b10ab18b61a620066a2236f0e948f0fd
橋本麟太郎 <rin.910491@gmail.com>
2026-03-16 11:47:09 +0900

    implement BooksController with index and show actions

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::BooksController < ApplicationController
+    before_action :set_book, only: [:show]
+
+    def index
+        books = Book.all
+        render json: { books: books }
+    end
+
+    def show
+        render json: { book: @book }
+    end
+
+    private
+
+    def set_book
+        @book = Book.find_by!(:slug params[:slug])
+    end
+end
